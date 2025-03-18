41be03e4fb103e9426cb30f3adc9469c413ccbde
Denis0110011 <ozhegov.den0110011@gmail.com>
2025-03-18 10:28:51 +0300

    http

--- index.php
+++ index.php
@@ -27,39 +27,35 @@ if ($mode === 'cli') {
         echo "delete Удалить пользователя <id>\n";
         exit(1);
     }
-
-
-    if ($mode === 'cli') {
-        $command = $argv[1];
-        switch ($command) {
-            case 'show':
-                $users = $UserManager->ShowUsers();
-                foreach ($users as $user) {
-                    echo ' id:' . $user['id'] . ' name:' . $user['name'] . ' email:' . $user['email'] . "\n";
-                }
-                break;
-            case 'add':
-                if (isset($argv[2], $argv[3])) {
-                    $name = $argv[2];
-                    $email = $argv[3];
-                    $UserID = $UserManager->CreateUser($name, $email);
-                    echo 'Добавлен пользователь:' . $UserID;
-                } else {
-                    echo 'Укажите имя и e-mail';
-                }
-                break;
-            case 'delete':
-                if (isset($argv[2])) {
-                    $id = (int) $argv[2];
-                    $UserID = $UserManager->DeleteUser($id);
-                    echo 'Удален пользователь:' . $UserID;
-                } else {
-                    echo 'Укажите id';
-                }
-                break;
-        }
-        exit;
+    $command = $argv[1];
+    switch ($command) {
+        case 'show':
+            $users = $UserManager->ShowUsers();
+            foreach ($users as $user) {
+                echo ' id:' . $user['id'] . ' name:' . $user['name'] . ' email:' . $user['email'] . "\n";
+            }
+            break;
+        case 'add':
+            if (isset($argv[2], $argv[3])) {
+                $name = $argv[2];
+                $email = $argv[3];
+                $UserID = $UserManager->CreateUser($name, $email);
+                echo 'Добавлен пользователь:' . $UserID;
+            } else {
+                echo 'Укажите имя и e-mail';
+            }
+            break;
+        case 'delete':
+            if (isset($argv[2])) {
+                $id = (int) $argv[2];
+                $UserID = $UserManager->DeleteUser($id);
+                echo 'Удален пользователь:' . $UserID;
+            } else {
+                echo 'Укажите id';
+            }
+            break;
     }
+    exit;
 }
 
 $app = AppFactory::create();
